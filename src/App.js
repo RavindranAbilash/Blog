887257dd5e7840c7eb4944333e7ebcce2316571e
abilash <e18013@eng.pdn.ac.lk>
2021-12-22 20:46:25 +0530

    write page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Topbar/>
-       <Single/>
+       <Write/>
     </>
   );
 }
